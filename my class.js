fdbf27c928abf7a3122afe09a8b027ac46352ed5
ASHISHROMAN <65939422+ASHISHROMAN@users.noreply.github.com>
2020-05-26 14:25:00 +0545

    ADD

---
+++ my class.js
@@ -0,0 +1,80 @@
+function ashish(){
+    var student=document.getElementById("txt").value 
+    var txt="";
+    var name;
+    if(student == "Aarty chaudhary"){
+name= " Name:Aarty Chaudhary<br> Address:Kailali<br>Roll:1 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Amar krishna hamal"){
+        name=" Name:Amar krishna hamal<br> Address:Tulsipur<br>Roll:2<br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Amrita khatri"){
+        name=" Name:Amrita khatri<br> Address:Tulsipur<br>Roll:3<br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Anand budha magar"){
+        name=" Name:Anand budha magar<br> Address:lamahi dang<br>Roll:4 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student=="Ashish kc"){
+        name=" Name:Ashish kc<br> Address:Ghorahi dang<br>Roll:5 <br>facuilty:DCOM<br>Batch:2074";
+        
+     }
+    else if(student == "Bir bahadur basnet"){
+        name=" Name:Bir bhadur basnet<br> Address:Tulsipur dang<br>Roll:6 <br>facuilty:DCOM<br>Batch:2074";
+        
+    }
+    else if(student == "Dipendra kc"){
+        name=" Name:Dipendra kc<br> Address:Salyan<br>Roll:7 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Gaurav bk"){
+        name=" Name:Gaurav bk<br> Address:Pyuthan<br>Roll:8 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Gopal thapa"){
+        name=" Name:Gopal thapa<br> Address:Nepalgunj<br>Roll:9 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Kamal chudara"){
+        name =" Name:Kamal chudara<br> Address:Pyuthan<br>Roll:10 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Madhusudan pun magar"){
+        name =" Name:Madhu sudan pun magar<br> Address:Bijauri Dang<br>Roll:11 <br>facuilty:DCOM<br>Batch:2074";
+        }
+    else if(student == "Kiran budha magar"){
+        name =" Name:Kiran buda magar<br> Address:Rukum<br>Roll:12 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Nabin bc"){
+        name=" Name:Nabin budhathoki<br> Address:Salyan<br>Roll:13 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Proovin pun"){
+        name=" Name:Proovin pun magar<br> Address:Ghorahi dang<br>Roll:15 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Nirajan kc"){
+        name=" Name:Nirajan kc<br> Address:Tulsipur dang<br>Roll:14 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Ram prasad bhattari"){
+        name="Name:Ram prasad bhattari<br> Address:Pyuthan<br>Roll:16 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Ramit sunar"){
+        name="Name:Ramit sunar<br> Address:Pyuthan<br>Roll:17<br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Sandesh pariyar"){
+        name="Name:Sandesh pariyar<br> Address:pashim dang<br>Roll:18 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student =="Saugat acharya"){
+        name="Name:Saugat acharya kc<br> Address:Ghorahi dang<br>Roll:19 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student =="Sher bahadur budha magar"){
+        name="Name:Sher bhadur budha<br> Address:Tulsipur dang<br>Roll:20 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student =="Suresh luhar"){
+        name="Name:Suresh luhar<br> Address:Kalikot<br>Roll:21 <br>facuilty:DCOM<br>Batch:2074";
+    }
+    else if(student == "Tika budhathoki"){
+        name="Name:Tika budhathoki<br> Address:Tulsipur dang<br>Roll:22 <br>facuilty:DCOM<br>Batch:2074";
+        
+        
+    }
+    
+    document.getElementById("d1").innerHTML=name
+    
+    
+    
+}
